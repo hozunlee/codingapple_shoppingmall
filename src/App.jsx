d19e9066523ee94022e602349c7b3026df3a2275
ho2yahh <ho2yahh@gmail.com>
2022-10-19 16:41:51 +0900

    setting : cra to Vite mygration

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import useStore from "./store/store";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import Main from "./page/Main";
+import Detail from "./page/Detail";
+
+import context1 from "./store/context";
+import { useState } from "react";
+
+function App() {
+    const [재고, 재고변경] = useState([10, 20, 30]);
+    const { shoesData1 } = useStore(); // 광역 상태관리
+    return (
+        <div className="App">
+            <NavBar />
+            <context1.Provider value={{ 재고 }}>
+                <Routes>
+                    <Route path="*" element={<Main />} />
+                    <Route path="/" element={<Main />} />
+                    <Route
+                        path="/detail/:id"
+                        element={<Detail data={shoesData1} />}
+                    />
+                </Routes>
+            </context1.Provider>
+        </div>
+    );
+}
+
+export default App;
